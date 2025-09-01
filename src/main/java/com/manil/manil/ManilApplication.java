3376aa29e801a104ede3574dce70fdb97b9aeb52
Kimhyochan <hyochan0220@gmail.com>
2025-09-01 19:38:58 +0900

    fix: - 올라가지 않았던 파일 수정

---
+++ src/main/java/com/manil/manil/ManilApplication.java
@@ -0,0 +1,13 @@
+package com.manil.manil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManilApplication.class, args);
+    }
+
+}
