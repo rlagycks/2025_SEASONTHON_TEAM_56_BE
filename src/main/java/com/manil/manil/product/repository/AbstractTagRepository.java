be3a8affd05e2e7f4a9ee1b343cd8f45b0c9788d
hyochan <106391445+rlagycks@users.noreply.github.com>
2025-09-07 07:01:21 +0900

    feat : 추천 로직 수정 - 추천 로직 수정

--- src/main/java/com/manil/manil/product/repository/AbstractTagRepository.java
+++ src/main/java/com/manil/manil/product/repository/AbstractTagRepository.java
@@ -5,11 +5,12 @@ import com.manil.manil.product.entity.AbstractTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface AbstractTagRepository extends JpaRepository<AbstractTag, Long> {
     List<AbstractTag> findByProductId(Long productId);
-
+    List<AbstractTag> findByProductIdIn(Collection<Long> productIds);
     @Modifying
     void deleteByProductId(Long productId);
 }
