78f6ff82110d8f5892fd614759bde842fcb55067
hyochan <106391445+rlagycks@users.noreply.github.com>
2025-09-07 04:38:05 +0900

    refactor : 추천 로직 동작 방식 수정 - 추천 로직 동작 방식 수정

--- src/main/java/com/manil/manil/product/repository/EmbeddingRepository.java
+++ src/main/java/com/manil/manil/product/repository/EmbeddingRepository.java
@@ -4,6 +4,9 @@ package com.manil.manil.product.repository;
 import com.manil.manil.product.entity.Embedding;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmbeddingRepository extends JpaRepository<Embedding, Long> {
     boolean existsByProduct_Id(Long productId);
+    Optional<Embedding> findByProduct_Id(Long productId);
 }
